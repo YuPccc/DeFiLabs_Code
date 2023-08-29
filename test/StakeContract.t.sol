d516a64d11ce9f56a871f7bc72f23ba5215de8a0
Paul Yu <pyu4203@gmail.com>
2023-08-29 14:21:42 +0800

    changes

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:  MIT
+pragma solidity ^0.8.10;
+
+import "ds-test/test.sol";
+import "../src/StakeContract.sol";
+import "../src/mocks/MockERC20.sol";
+
+contract StakeContractTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}  
